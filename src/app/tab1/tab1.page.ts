ec4687ab701c504ce22268349266964434e783f2
colten995 <Cgoetz9919@conestogac.on.ca>
2025-05-13 16:38:16 -0400

    implemented code functionality in tab 1 and 2 that doesn't required old cordova plugins

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,106 @@
 import { Component } from '@angular/core';
+import {FishManagerService, Fish} from '../fish-manager.service';
+// import {AlertController} from '@ionic/angular';
+// import {Camera, CameraOptions} from '@ionic-native/camera/ngx';
+// import { Geolocation } from '@ionic-native/geolocation/ngx';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  standalone: false,
+  //Declares the component as an old ngmodule instead of a standalone component https://v17.angular.io/guide/standalone-components 
+  standalone: false
 })
 export class Tab1Page {
 
-  constructor() {}
+  /*The model for each fish caught includes:
+    Species : the species of the fish ex: Bass
+    Weight : The measured weight of the fish
+    Length : The measured length of the fish
+    Latitude : The latitude of the device when the fish was caught
+    Longitude : The longitude of the device when the fish was caught
+    Image : The path of the image taken of the fish
+    Date : The current date
+    Name : A name for the fish
+  */
+    
+    //TODO: create function to initialize an empty fish, is this possible? Fish is an interface
+  public fishToAdd: Fish = {
+    species:"",
+    weight: 0,
+    length: 0,
+    latitude: 0,
+    longitude: 0,
+    image:"",
+    date: new Date().toLocaleString('en-US', {timeZone : 'UTC'}),
+    name: ""
+  };
 
+  /*It's required to initialize all of these classes in the ctor to use them. Angular determines which dependencies this component needs
+  by looking at the constructor of the component */
+  
+    // private alertController : AlertController,
+    // private camera : Camera,
+    // private geoLocation : Geolocation
+  constructor(public fishManager: FishManagerService) {
+      }
+
+  //  public getFishingSpot()
+  //   {
+  //     //use an async call to get the current position with the GeoLocation plugin
+  //     this.geoLocation.getCurrentPosition().then((response) => {
+  //       this.fishToAdd.latitude = response.coords.latitude;
+  //       this.fishToAdd.longitude = response.coords.longitude;
+  //       //Display an error alert box if the current position can't be retrieved
+  //      }).catch( async (error) => {
+  //        const errorAlert = await this.alertController.create({
+  //          message : 'Error getting location' + error,
+  //          buttons: ['OK']
+  //        });
+  //        errorAlert.present();
+  //      });
+  //   }
+    
+  //  public takePicture()
+  //   {
+  //     const options: CameraOptions = {
+  //       quality: 100,
+  //       destinationType: this.camera.DestinationType.DATA_URL,
+  //       targetHeight: 100,
+  //       targetWidth: 100,
+  //       encodingType: this.camera.EncodingType.JPEG,
+  //       mediaType: this.camera.MediaType.PICTURE
+  //     };
+  //   this.camera.getPicture(options).then((imageData) => {
+  //    this.fishToAdd.image = "data:image/jpeg;base64," + imageData;
+  //   }, (err) => {
+  //   });
+  // }
+
+  public async validateAndSubmitForm(form: HTMLFormElement) {
+    // let alert;
+    if (form.reportValidity()) {
+      this.fishManager.addToCollection(this.fishToAdd);
+      //Debug message -- remove later
+      alert(`Species : ${this.fishToAdd.species}, Name: ${this.fishToAdd.name}, Weight: ${this.fishToAdd.weight},
+         Length: ${this.fishToAdd.length}, Latitude: ${this.fishToAdd.latitude}, Longitude: ${this.fishToAdd.longitude},
+          Date Caught: ${this.fishToAdd.date}`);
+      //call function to initialize new empty fish
+        this.fishToAdd = {
+          species:"",
+          weight: 0,
+          length: 0,
+          latitude:0,
+          longitude:0,
+          image:"",
+          date: new Date().toLocaleString('en-US', {timeZone : 'UTC'}),
+          name: ""
+        };
+        // alert = await this.alertController.create({
+        //   message: 'Fish succesfully added to collection',
+        //   buttons: ['OK']
+        // });
+        // alert.present();
+      }
+  }
 }
--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import {FishManagerService, Fish} from '../fish-manager.service';
+// import { AlertController } from '@ionic/angular';
+
 
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss'],
-  standalone: false,
+  standalone: false
 })
 export class Tab2Page {
 
-  constructor() {}
+    // private alertController:AlertController
+  constructor(public fishManager : FishManagerService) {}
 
+    public async presentFishInformation(fishToPresent:Fish) {
+      //Debug message -- remove later
+            alert(`Species : ${fishToPresent.species}, Name: ${fishToPresent.name}, Weight: ${fishToPresent.weight},
+         Length: ${fishToPresent.length}, Latitude: ${fishToPresent.latitude}, Longitude: ${fishToPresent.longitude},
+          Date Caught: ${fishToPresent.date}`);
+      // const fishInformationAlert = await this.alertController.create(
+      //   {
+      //     message:
+      //     'Name: ' + fishToPresent.name + '<br/>' +
+      //     'Species: ' + fishToPresent.species + '<br/>' +
+      //     'Weight: ' + fishToPresent.weight + this.fishManager.weightUnit + '<br/>' +
+      //     'Length: ' + fishToPresent.length + this.fishManager.measurementSystem + '<br/>' +
+      //     'Latitude: ' + fishToPresent.latitude + '<br/>' +
+      //     'Longitude: ' + fishToPresent.longitude + '<br/>' +
+      //     'Date Caught: ' + fishToPresent.date,
+      //     buttons: ['OK']
+      //   }
+      // );
+      // fishInformationAlert.present();
+    }   
 }
