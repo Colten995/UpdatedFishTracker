08adc5f12d8219af5fd5b974d5a8f55427209a2c
colten995 <Cgoetz9919@conestogac.on.ca>
2025-05-13 16:02:15 -0400

    Commented out functions in fish manager that rely on old cordova plugins

--- src/app/fish-manager.service.ts
+++ src/app/fish-manager.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import {Storage} from '@ionic/storage';
+// import {Storage} from '@ionic/storage';
 import {AlertController} from '@ionic/angular';
 
 //This is the key that will have the settings values
@@ -19,78 +19,79 @@ export class FishManagerService {
   measurementSystem = 'in';
   fishes: Array<Fish> = [];
 
-  constructor(private storage: Storage,
-    private alertController : AlertController) 
+  // private storage: Storage,
+    // private alertController : AlertController
+  constructor() 
   {
-    this.initialize();
+    // this.initialize();
   }
 
   //Initialize the fish manager
-  public async initialize()
-  {
-    //Get the settings from the db and assign it to the setting object
-   const settings = await this.storage.get(SETTINGSKEY);
-   let savedFishes = await this.storage.get(FISHKEY);
+  // public async initialize()
+  // {
+  //   //Get the settings from the db and assign it to the setting object
+  //  const settings = await this.storage.get(SETTINGSKEY);
+  //  let savedFishes = await this.storage.get(FISHKEY);
 
-   //Make sure settings is not null
-   if (settings)
-   {
-      this.clearDbOnStartup = settings.clearDbOnStartup;
-      this.weightUnit = settings.weightUnit;
-      this.measurementSystem = settings.measurementSystem;
-      if (this.clearDbOnStartup)
-      {
-       this.clearDatabase();
-       savedFishes = [];
-      }
-   }
-   if (savedFishes)
-   {
-     this.fishes = savedFishes;
-   }
+  //  //Make sure settings is not null
+  //  if (settings)
+  //  {
+  //     this.clearDbOnStartup = settings.clearDbOnStartup;
+  //     this.weightUnit = settings.weightUnit;
+  //     this.measurementSystem = settings.measurementSystem;
+  //     if (this.clearDbOnStartup)
+  //     {
+  //      this.clearDatabase();
+  //      savedFishes = [];
+  //     }
+  //  }
+  //  if (savedFishes)
+  //  {
+  //    this.fishes = savedFishes;
+  //  }
 
-  }
+  // }
 
   //Saves the new settings to the db
-  public async saveSettings()
-  {
-    const savedAlert = await this.alertController.create(
-      {
-        message: 'Settings saved',
-        buttons:['OK']
-      }
-    )
-    //Make a new object for the new settings
-    const newSettings =
-    {
-      clearDbOnStartup : this.clearDbOnStartup,
-      weightUnit : this.weightUnit,
-      measurementSystem : this.measurementSystem
-    }
-    this.storage.set(SETTINGSKEY, newSettings);
-    savedAlert.present();
-  }
+  // public async saveSettings()
+  // {
+  //   const savedAlert = await this.alertController.create(
+  //     {
+  //       message: 'Settings saved',
+  //       buttons:['OK']
+  //     }
+  //   )
+  //   //Make a new object for the new settings
+  //   const newSettings =
+  //   {
+  //     clearDbOnStartup : this.clearDbOnStartup,
+  //     weightUnit : this.weightUnit,
+  //     measurementSystem : this.measurementSystem
+  //   }
+  //   this.storage.set(SETTINGSKEY, newSettings);
+  //   savedAlert.present();
+  // }
 
   /*
     Clears the database and logs the fish that were cleared
   */
-  public async clearDatabase()
-  {
-    const clearAlert = await this.alertController.create(
-      {
-        message: 'Collection cleared',
-        buttons:['OK']
-      }
-    )
-    this.storage.remove(FISHKEY);
-    this.fishes = [];
-    clearAlert.present();
-  }
+  // public async clearDatabase()
+  // {
+  //   const clearAlert = await this.alertController.create(
+  //     {
+  //       message: 'Collection cleared',
+  //       buttons:['OK']
+  //     }
+  //   )
+  //   this.storage.remove(FISHKEY);
+  //   this.fishes = [];
+  //   clearAlert.present();
+  // }
 
   public addToCollection(fishToAdd: Fish)
   {
         this.fishes.push(fishToAdd);
-        this.storage.set(FISHKEY, this.fishes);
+        // this.storage.set(FISHKEY, this.fishes);
   }
 
 }
